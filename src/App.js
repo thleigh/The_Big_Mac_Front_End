477193c85a99b46fa66beae8c212ef172239193a
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-19 20:08:49 -0700

    [css] initialize beginning css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>The Big Mac Project</h1>
+      <div className="container">
+        <h1>The Big Mac Project</h1>
+        <p>The Big Mac Project compares the price of the Bic Mac Meal in every Major City in America.</p>
+
+        <p>Why is this important?</p>
+      </div>
     </div>
   );
 }
