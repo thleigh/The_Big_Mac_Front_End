16ca1a1bf83892d8602f2bb37424f84181fa0eb6
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-21 19:01:26 -0700

    [welcome] build welcome page with big mac image and landing page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/data" component={Data} />
-          
       </div>
     </div>
   );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,14 @@
 import React from "react"
+import bigmac from '../assets/big-mac.png'
 
 const Welcome = (props) => {
 
     return(
         <div>
-            <p>Welcome</p>
+            <img src={bigmac}></img>
+            <h3>Welcome to The Big Mac Project</h3>
+            <p>The Big Mac Project compares every Big Mac price in every major city across the US.</p>
+            <button className='btn btn-info'> Get Started </button>
         </div>
     )
 }
