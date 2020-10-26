df2598a650b6bef0327b46804dd1c3e84e8d1c52
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-26 16:58:26 -0700

    [data.js] create axios call to pull data from server

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,17 +1,23 @@
 import React, {useState, useEffect} from "react"
 import axios from 'axios'
+const BIG_MAC_SERVER_URL = process.env.BIG_MAC_SERVER_URL;
 
 const Data = (props) => {
     let [location, setLocation] = useState([]);
     let [price, setPrice] = useState([]);
 
     useEffect(() => {
-        axios.get(`${localhost}`)
-    })
+        axios.get(`${BIG_MAC_SERVER_URL}/api/bigmacs/`)
+        .then(response => {
+            setLocation(response.data[0])
+        })
+        .catch(err => console.log(err))
+    }, [])
 
     return(
         <div>
             <p>Data</p>
+            <p>Location: {location}</p>
         </div>
     )
 }
