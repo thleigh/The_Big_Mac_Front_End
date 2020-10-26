042a91808bcecfa94c28fe9c7e40399f44758526
Tanner Leigh <tannerhleigh@gmail.com>
2020-10-26 16:21:24 -0700

    [data.js] add use state and use effect with the inital setprice and setlocation

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,13 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import axios from 'axios'
 
 const Data = (props) => {
+    let [location, setLocation] = useState([]);
+    let [price, setPrice] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${localhost}`)
+    })
 
     return(
         <div>
